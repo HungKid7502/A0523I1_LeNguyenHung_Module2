cf3e4aebdba51bc9bec0993ff7ee1f5366911d2a
hungkid7502 <hunglnde160537@fpt.edu.vn>
2023-11-01 05:02:00 +0700

    hungcuti

---
+++ module2/src/ss4_Class_and_Object_in_Java/QuadraticEquation/Main.java
@@ -0,0 +1,26 @@
+package ss4_Class_and_Object_in_Java.QuadraticEquation;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a;
+        do {
+            System.out.println("Enter coefficient a (a != 0): ");
+            a = Double.parseDouble(sc.nextLine());
+        } while (a == 0);
+        System.out.println("Enter coefficient b: ");
+        double b = Double.parseDouble(sc.nextLine());
+        System.out.println("Enter coefficient c: ");
+        double c = Double.parseDouble(sc.nextLine());
+        QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
+        if (quadraticEquation.getDiscriminant() < 0) {
+            System.out.println("The equation has no roots");
+        } else if (quadraticEquation.getDiscriminant() == 0) {
+            System.out.println("The equation has one root is x = " + quadraticEquation.getRoot3());
+        } else {
+            System.out.println("The equation has two roots is x1 = " + quadraticEquation.getRoot1()
+                    + " and x2 = " + quadraticEquation.getRoot2());
+        }
+    }
+}
---
+++ module2/src/ss4_Class_and_Object_in_Java/QuadraticEquation/QuadraticEquation.java
@@ -0,0 +1,40 @@
+package ss4_Class_and_Object_in_Java.QuadraticEquation;
+
+public class QuadraticEquation {
+    private double a, b, c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        double delta = Math.pow(b, 2) - (4 * a * c);
+        return delta;
+    }
+
+    public double getRoot1() {
+        return (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    }
+
+    public double getRoot2() {
+        return (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+    }
+
+    public double getRoot3() {
+        return -b / (2 * a);
+    }
+}
