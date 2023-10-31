51a2a0c0d610d3485e4ccb13a055410e8294c236
hungkid7502 <hunglnde160537@fpt.edu.vn>
2023-11-01 03:29:29 +0700

    hungcuti

--- module2/src/ss3_Array_And_Method_in_Java/exercise/AddElement.java
+++ module2/src/ss3_Array_And_Method_in_Java/exercise/AddElement.java
@@ -1,4 +1,38 @@
 package ss3_Array_And_Method_in_Java.exercise;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class AddElement {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter the size: ");
+            size = sc.nextInt();
+            if (size > 10)
+                System.out.println("Size does not exceed 10");
+        } while (size > 10);
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Enter element " + (i + 1) + ": ");
+            array[i] = sc.nextInt();
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        System.out.println(Arrays.toString(array));
+        int indexAdd;
+        do {
+            System.out.println("Enter position to be additional: ");
+            indexAdd = sc.nextInt();
+        } while (indexAdd <= -1 || indexAdd > array.length - 1);
+        System.out.println("Enter element to be additional");
+        int elementAdd = sc.nextInt();
+        for (int i = array.length - 1; i > indexAdd; i--) {
+            array[i] = array[i - 1];
+        }
+        array[indexAdd] = elementAdd;
+        System.out.printf("%-20s%s", "Element in array after additional: ", "");
+        System.out.println(Arrays.toString(array));
+    }
 }
--- module2/src/ss3_Array_And_Method_in_Java/exercise/DeleteElement.java
+++ module2/src/ss3_Array_And_Method_in_Java/exercise/DeleteElement.java
@@ -1,4 +1,40 @@
 package ss3_Array_And_Method_in_Java.exercise;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class DeleteElement {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner sc = new Scanner(System.in);
+        do {
+            System.out.println("Enter the size: ");
+            size = sc.nextInt();
+            if (size > 10)
+                System.out.println("Size does not exceed 10");
+        } while (size > 10);
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Enter element " + (i + 1) + ": ");
+            array[i] = sc.nextInt();
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        System.out.println(Arrays.toString(array));
+        int indexDel;
+        int elementDel;
+        System.out.println("Enter the element you want to delete: ");
+        elementDel = sc.nextInt();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == elementDel) {
+                indexDel = i;
+                System.out.println("The element at index " + indexDel);
+                for (int j = indexDel; j < array.length - 1; j++) {
+                    array[j] = array[j + 1];
+                }
+                array[array.length - 1] = 0;
+            }
+        }
+        System.out.println("Array after delete:");
+        System.out.println(Arrays.toString(array));
+    }
 }
